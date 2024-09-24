85fbd094c43653ec5ac1ec03aef8aab5a5cc2e70
Nahuel Rodriguez <nahufed@gmail.com>
2024-09-24 18:06:44 -0300

    agregados los controllers para certificados

--- Backend/Routes/certificados.js
+++ Backend/Routes/certificados.js
@@ -2,13 +2,13 @@ const express = require("express")
 
 const router = express.Router()// metodo propio de express que tiene el enrutamiento
 
-const {allCertificados,singleCertificados,createCertificados,editCertificados,deleteCertificados} = require("../controllers/certificados")
+const {allCertificados,singleCertificado,createCertificado,editCertificado,deleteCertificado} = require("../Controllers/certificados")
 
 //peticiones http
 router.get("/certificados/",allCertificados)//muestra todo
-router.get("/certificados/:id", singleCertificados)//para ver uno
-router.post("/certificados/create",createCertificados)
-router.put("/certificados/edit/:id",editCertificados)
-router.put("/certificados/delete/:id",deleteCertificados)
+router.get("/certificados/:id", singleCertificado)//para ver uno
+router.post("/certificados/create",createCertificado)
+router.put("/certificados/edit/:id",editCertificado)
+router.put("/certificados/delete/:id",deleteCertificado)
 
 module.exports = router
