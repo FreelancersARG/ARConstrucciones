69b99e7817a21d343afe369189f96de55763a603
Nahuel Rodriguez <nahufed@gmail.com>
2024-09-24 21:54:46 -0300

    corregido errores en viajes y testeado

--- Backend/Controllers/viajes.js
+++ Backend/Controllers/viajes.js
@@ -3,7 +3,7 @@ const {conection} = require('../DB/config');
 // Funcion para mostrar todos los viajes
 
 const allViajes = (req, res) => {
-    const query = `select * from Viajes;`
+    const query = `select * from Viajes where activoViaje = 1;`
     conection.query(query, (err, results) => {
         if (err) throw err;
         res.json(results);
@@ -12,7 +12,7 @@ const allViajes = (req, res) => {
 
 //Funcion para mostrar un viaje en particular
 
-const singleViajes = (req, res) => {
+const singleViaje = (req, res) => {
     const id = req.params.id;
     const query = `select * from Viajes where id_viaje = ${id};`
     conection.query(query, (err, results) => {
@@ -23,7 +23,7 @@ const singleViajes = (req, res) => {
 
 //Funcion para editar un viaje
 
-const editViajes = (req, res) => {
+const editViaje = (req, res) => {
     const {fechaViaje, id_obra, id_vehiculo, activoViaje} = req.body;
     const id = req.params.id;
     const query = `update Viajes set fechaViaje= '${fechaViaje}', id_obra= '${id_obra}', id_vehiculo= ${id_vehiculo}, activoViaje= ${activoViaje} where id_viaje = ${id};`
@@ -35,18 +35,21 @@ const editViajes = (req, res) => {
 
 //Funcion para crear un viaje
 
-const createViajes = (req, res) => {
+const createViaje = (req, res) => {
     const {fechaViaje, id_obra, id_vehiculo, activoViaje} = req.body;
     const query = `insert into Viajes (fechaViaje, id_obra, id_vehiculo, activoViaje) values ('${fechaViaje}', ${id_obra}, ${id_vehiculo}, ${activoViaje});`
     conection.query(query, (err, results) => {
         if (err) throw err;
-        res.send(results)
+        res.json({
+            message: "Viaje creado exitosamente",
+            id: results.insertId,
+          });
     })
 }
 
 //Funcion para eliminar un viaje
 
-const deleteViajes = (req, res) => {
+const deleteViaje = (req, res) => {
     const id = req.params.id;
     const query = `update Viajes set activoViaje = 0 where id_viaje = ${id};`
     conection.query(query, (err, results) => {
@@ -55,4 +58,4 @@ const deleteViajes = (req, res) => {
     })
 }
 
-module.exports = {allViajes, singleViajes, createViajes, editViajes, deleteViajes}
+module.exports = {allViajes, singleViaje, createViaje, editViaje, deleteViaje}
