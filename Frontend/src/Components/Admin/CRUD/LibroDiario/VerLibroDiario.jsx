f133089e0c25b61e6e2146c100dfd7b0f67d6fc7
lucianodevs <cordobes127@hotmail.com>
2024-12-20 03:36:47 -0300

    link en el alert pendiente

--- Frontend/src/Components/Admin/CRUD/LibroDiario/VerLibroDiario.jsx
+++ Frontend/src/Components/Admin/CRUD/LibroDiario/VerLibroDiario.jsx
@@ -82,7 +82,7 @@ useEffect(() => {
       const obtenerPendientes = async () => {
         try {
           const response = await axios.get(`${URL_PENDIENTES}`, {
-            headers: { Authorization: `Bearer ${token}` },
+        headers: { Authorization: `Bearer ${token}` },
           });
           const pendientes = response.data;
       
@@ -90,19 +90,29 @@ useEffect(() => {
           const tareasPendientes = pendientes.filter((tarea) => tarea.estado === 'Pendiente');
       
           if (tareasPendientes.length > 0) {
-            toast(`Tienes ${tareasPendientes.length} tareas pendientes.`, {
-              icon: '🔔', // Icono de la notificación
-              position: 'top-right', // Posición de la notificación
-            });
+        toast(
+          (t) => (
+            <span>
+          Tienes {tareasPendientes.length} tareas pendientes.{' '}
+          <Link to="/Admin/Pendientes" onClick={() => toast.dismiss(t.id)} className="text-blue-500 underline">
+            Ver Pendientes
+          </Link>
+            </span>
+          ),
+          {
+            icon: '🔔', // Icono de la notificación
+            position: 'top-right', // Posición de la notificación
+          }
+        );
           } else {
-            toast.info('No tienes tareas pendientes.', {
-              position: 'top-right',
-            });
+        toast.info('No tienes tareas pendientes.', {
+          position: 'top-right',
+        });
           }
         } catch (error) {
           console.error('Error al obtener las tareas pendientes:', error);
           toast.error('Error al cargar las tareas pendientes.', {
-            position: 'top-right',
+        position: 'top-right',
           });
         }
       };
